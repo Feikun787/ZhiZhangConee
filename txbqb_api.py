79d7202a572f9bd3f98e31bb7b42ab4b416520f7
Feikun787 <3451263590@qq.com>
2025-12-18 14:27:51 +0800

    Add files via upload

---
+++ txbqb_api.py
@@ -0,0 +1,330 @@
+
+
+# 合成方法字典映射
+# 反向映射字典（中文到英文）
+MEME_METHODS = {
+    "5000兆": "5000choyen",
+    "戒导": "abstinence",
+    "逆转裁判气泡": "ace_attorney_dialog",
+    "二次元入口": "acg_entrance",
+    "添乱": "add_chaos",
+    "给社会添乱": "add_chaos",
+    "上瘾": "addiction",
+    "毒瘾发作": "addiction",
+    "一样": "alike",
+    "支付宝支付": "alipay",
+    "一直": "always",
+    "我永远喜欢": "always_like",
+    "防诱拐": "anti_kidnap",
+    "阿尼亚喜欢": "anya_suki",
+    "鼓掌": "applaud",
+    "阿罗娜扔": "arona_throw",
+    "升天": "ascension",
+    "问问": "ask",
+    "亚托莉枕头": "atri_pillow",
+    "宁宁举牌": "ayachi_holdsign",
+    "ba说": "ba_say",
+    "继续干活": "back_to_work",
+    "打工人": "back_to_work",
+    "后空翻": "backflip",
+    "悲报": "bad_news",
+    "拍头": "beat_head",
+    "揍": "beat_up",
+    "啃": "bite",
+    "真寻挨骂": "blamed_mahiro",
+    "高血压": "blood_pressure",
+    "蔚蓝档案标题": "bluearchive",
+    "batitle": "bluearchive",
+    "波奇手稿": "bocchi_draft",
+    "布洛妮娅举牌": "bronya_holdsign",
+    "大鸭鸭举牌": "bronya_holdsign",
+    "奶茶": "bubble_tea",
+    "遇到困难请拨打": "call_110",
+    "咖波画": "capoo_draw",
+    "咖波指": "capoo_point",
+    "咖波撕": "capoo_rip",
+    "咖波蹭": "capoo_rub",
+    "咖波贴": "capoo_rub",
+    "咖波说": "capoo_say",
+    "咖波炖": "capoo_stew",
+    "咖波撞": "capoo_strike",
+    "咖波头槌": "capoo_strike",
+    "舰长": "captain",
+    "这个引起的": "caused_by_this",
+    "奖状": "certificate",
+    "证书": "certificate",
+    "馋身子": "chanshenzi",
+    "字符画": "charpic",
+    "追列车": "chase_train",
+    "追火车": "chase_train",
+    "国旗": "china_flag",
+    "智乃扔": "chino_throw",
+    "智乃抛": "chino_throw",
+    "鼠鼠搓": "clauvio_twist",
+    "小丑": "clown",
+    "小丑面具": "clown_mask",
+    "迷惑": "confuse",
+    "兑换券": "coupon",
+    "捂脸": "cover_face",
+    "爬": "crawl",
+    "群青": "cyan",
+    "白天黑夜": "daynight",
+    "白天晚上": "daynight",
+    "像样的亲亲": "decent_kiss",
+    "入典": "dianzhongdian",
+    "典中典": "dianzhongdian",
+    "黑白草图": "dianzhongdian",
+    "恐龙": "dinosaur",
+    "小恐龙": "dinosaur",
+    "注意力涣散": "distracted",
+    "离婚协议": "divorce",
+    "离婚申请": "divorce",
+    "狗都不玩": "dog_dislike",
+    "管人痴": "dog_of_vtb",
+    "不要靠近": "dont_go_near",
+    "不要按": "dont_press",
+    "别碰": "dont_touch",
+    "douyin": "douyin",
+    "吃": "eat",
+    "皇帝龙图": "emperor_dragon",
+    "意若思镜": "erised_mirror",
+    "灰飞烟灭": "fade_away",
+    "狂爱": "fanatic",
+    "狂粉": "fanatic",
+    "闭嘴": "father_work",
+    "我爸爸": "father_work",
+    "击剑": "fencing",
+    "我打宿傩": "fight_with_sunuo",
+    "我打宿傩吗": "fight_with_sunuo",
+    "满脑子": "fill_head",
+    "整点薯条": "find_chips",
+    "流萤举牌": "firefly_holdsign",
+    "闪瞎": "flash_blind",
+    "弹": "flick",
+    "脑瓜崩": "flick",
+    "红温": "flush",
+    "回南天": "fogging",
+    "水雾": "fogging",
+    "关注": "follow",
+    "芙莉莲拿": "frieren_take",
+    "哈哈镜": "funny_mirror",
+    "垃圾": "garbage",
+    "垃圾桶": "garbage",
+    "原神吃": "genshin_eat",
+    "原神启动": "genshin_start",
+    "喜报": "good_news",
+    "google": "google",
+    "谷歌验证码": "google_captcha",
+    "猩猩扔": "gorilla_throw",
+    "鬼畜": "guichu",
+    "手枪": "gun",
+    "锤": "hammer",
+    "凉宫春日举": "haruhi_raise",
+    "高低情商": "high_EQ",
+    "低高情商": "high_EQ",
+    "打穿": "hit_screen",
+    "打穿屏幕": "hit_screen",
+    "记仇": "hold_grudge",
+    "抱紧": "hold_tight",
+    "抱": "hug",
+    "抱抱": "hug",
+    "抱大腿": "hug_leg",
+    "胡桃啃": "hutao_bite",
+    "坐牢": "imprison",
+    "不文明": "incivilization",
+    "inside": "intel_inside",
+    "采访": "interview",
+    "杰瑞盯": "jerry_stare",
+    "假面骑士": "jiamianqishi",
+    "急急国王": "jiji_king",
+    "汐汐": "jinhsi",
+    "今汐": "jinhsi",
+    "旧病复发": "jiubingfufa",
+    "啾啾": "jiujiu",
+    "跳": "jump",
+    "万花筒": "kaleidoscope",
+    "万花镜": "kaleidoscope",
+    "凯露指": "karyl_point",
+    "远离": "keep_away",
+    "压岁钱不要交给": "keep_your_money",
+    "踢球": "kick_ball",
+    "卡比锤": "kirby_hammer",
+    "卡比重锤": "kirby_hammer",
+    "亲": "kiss",
+    "亲亲": "kiss",
+    "可莉吃": "klee_eat",
+    "敲": "knock",
+    "心奈印章": "kokona_seal",
+    "泉此方看": "konata_watch",
+    "偷学": "learn",
+    "左右横跳": "left_right_jump",
+    "让我进去": "let_me_in",
+    "舔糖": "lick_candy",
+    "舔棒棒糖": "lick_candy",
+    "等价无穷小": "lim_x_0",
+    "听音乐": "listen_music",
+    "小天使": "little_angel",
+    "加载中": "loading",
+    "看扁": "look_flat",
+    "看图标": "look_this_icon",
+    "循环": "loop",
+    "寻狗启事": "lost_dog",
+    "永远爱你": "love_you",
+    "洛天依要": "luotianyi_need",
+    "天依要": "luotianyi_need",
+    "洛天依说": "luotianyi_say",
+    "天依说": "luotianyi_say",
+    "罗永浩说": "luoyonghao_say",
+    "鲁迅说": "luxun_say",
+    "鲁迅说过": "luxun_say",
+    "真寻看书": "mahiro_readbook",
+    "麦克阿瑟说": "maikease",
+    "旅行伙伴觉醒": "maimai_awaken",
+    "旅行伙伴加入": "maimai_join",
+    "交个朋友": "make_friend",
+    "结婚申请": "marriage",
+    "结婚登记": "marriage",
+    "流星": "meteor",
+    "米哈游": "mihoyo",
+    "上香": "mourning",
+    "低语": "murmur",
+    "我朋友说": "my_friend",
+    "我的意见如下": "my_opinion",
+    "我的意见是": "my_opinion",
+    "我老婆": "my_wife",
+    "这是我老婆": "my_wife",
+    "纳西妲啃": "nahida_bite",
+    "草神啃": "nahida_bite",
+    "亚文化取名机": "name_generator",
+    "亚名": "name_generator",
+    "需要": "need",
+    "你可能需要": "need",
+    "猫羽雫举牌": "nekoha_holdsign",
+    "猫猫举牌": "nekoha_holdsign",
+    "你好骚啊": "nihaosaoa",
+    "伊地知虹夏举牌": "nijika_holdsign",
+    "虹夏举牌": "nijika_holdsign",
+    "无响应": "no_response",
+    "诺基亚": "nokia",
+    "有内鬼": "nokia",
+    "不喊我": "not_call_me",
+    "请假条": "note_for_leave",
+    "我推的网友": "oshi_no_ko",
+    "osu": "osu",
+    "out": "out",
+    "加班": "overtime",
+    "女神异闻录5预告信": "p5letter",
+    "P5预告信": "p5letter",
+    "这像画吗": "paint",
+    "小画家": "painter",
+    "熊猫龙图": "panda_dragon_figure",
+    "推锅": "pass_the_buck",
+    "甩锅": "pass_the_buck",
+    "拍": "pat",
+    "佩佩举": "pepe_raise",
+    "完美": "perfect",
+    "摸": "petpet",
+    "摸摸": "petpet",
+    "摸头": "petpet",
+    "rua": "petpet",
+    "捏": "pinch",
+    "捏脸": "pinch",
+    "像素化": "pixelate",
+    "pjsk": "pjsk",
+    "世界计划": "pjsk",
+    "普拉娜吃": "plana_eat",
+    "普拉娜舔": "plana_eat",
+    "顶": "play",
+    "玩": "play",
+    "打棒球": "play_baseball",
+    "打篮球": "play_basketball",
+    "火柴人打篮球": "play_basketball",
+    "玩游戏": "play_game",
+    "一起玩": "play_together",
+    "出警": "police",
+    "警察": "police1",
+    "为什么@我":'why_at_me'
+}
+#why_at_me (为什么@我)
+
+
+import os
+from flask import Flask, request, send_file, jsonify
+from meme_generator import get_meme
+from io import BytesIO
+import redis
+app = Flask(__name__)
+
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+
+
+def redis_set(id,type,image):
+    id = str( id)+str (type)
+    r.setex(id,3600,image.getvalue())
+
+def redis_get(id,type):
+    id = str( id)+str (type)
+    image = r.get(id)
+    if image:
+        return BytesIO(image)
+    else:
+        return None
+
+#判断redis是否可用
+@app.route('/generate_meme', methods=['POST'])
+def generate_meme(cqredis=True):
+    try:
+        # 获取请求参数
+        data = request.get_json()
+        id = data.get('id', '')
+        image_path = data.get('image_path', '')
+        meme_type = data.get('meme_type', '戒导')
+        circle = data.get('circle', True)
+
+        # 检查图片文件是否存在
+        if not os.path.exists(image_path):
+            return jsonify({'error': f'图片文件不存在: {image_path}'}), 400
+
+        # 生成 meme
+        result = None
+        if cqredis:
+            try:
+                result = redis_get(id,meme_type)
+            except:
+                cqredis = False
+                print("redis不可用000")
+
+        if not result:
+            meme = get_meme(MEME_METHODS[meme_type])
+            result = meme(images=[image_path], texts=[], args={"circle": circle})
+            try:
+                if cqredis:
+                    redis_set(id,meme_type,result)
+            except Exception as e:
+                cqredis = False
+                print(f"redis不可用{e}")
+
+        # 将结果保存到内存中
+        meme_buffer = BytesIO(result.getvalue())
+        meme_buffer.seek(0)
+
+        # 返回生成的 meme 文件
+        return send_file(
+            meme_buffer,
+            mimetype='image/gif',
+            as_attachment=True,
+            download_name='result.gif'
+        )
+
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500
+
+
+@app.route('/health', methods=['GET'])
+def health_check():
+    return jsonify({'status': 'ok'})
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=False)
